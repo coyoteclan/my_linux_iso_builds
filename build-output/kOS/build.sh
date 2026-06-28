50e473d4be37a60f52a517c7ff7ec0fc11091045
Mechamorph <118144070+coyoteclan@users.noreply.github.com>
2026-06-28 06:27:03 +0500

    Update build.sh

--- build-output/kOS/build.sh
+++ build-output/kOS/build.sh
@@ -128,17 +128,17 @@ lb build 2>&1 | tee /output/build.log
 echo "================================"
 echo "DIAGNOSTIC: Chroot size breakdown"
 echo "================================"
-du -sh /tmp/live-build/chroot/* | sort -h | tail -30 2>&1 | tee /output/sizes_log
-du -sh /tmp/live-build/chroot/usr/share/* | sort -h | tail -20 2>&1 | tee /output/sizes_log
+du -sh /tmp/live-build/chroot/* | sort -h | tail -30 2>&1 | tee /output/sizes_log1
+du -sh /tmp/live-build/chroot/usr/share/* | sort -h | tail -20 2>&1 | tee /output/sizes_log2
 for f in /tmp/live-build/chroot/usr/share/*; do
-  du -sh $f/* | sort -h | tail -20 2>&1 | tee /output/sizes_log
+  du -sh $f/* | sort -h | tail -20 2>&1 | tee /output/sizes_log3
 done
-du -sh /tmp/live-build/chroot/usr/lib/* | sort -h | tail -20 2>&1 | tee /output/sizes_log
+du -sh /tmp/live-build/chroot/usr/lib/* | sort -h | tail -20 2>&1 | tee /output/sizes_log4
 for f in /tmp/live-build/chroot/usr/lib/*; do
-  du -sh $f/* | sort -h | tail -20 2>&1 | tee /output/sizes_log
+  du -sh $f/* | sort -h | tail -20 2>&1 | tee /output/sizes_log5
 done
 
-du -hs /opt/* 2>&1 | tee /output/sizes_log
+du -hs /tmp/live-build/chroot/opt/* 2>&1 | tee /output/sizes_log6
 
 if [ -f binary/live/filesystem.squashfs ]; then
   echo "================================"
