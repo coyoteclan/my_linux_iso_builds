1baee6ee3045b1d33c642264481e3a52f88cd956
Mechamorph <118144070+coyoteclan@users.noreply.github.com>
2026-06-29 20:42:08 +0500

    Update build.sh

--- build-output/kOS/build.sh
+++ build-output/kOS/build.sh
@@ -99,9 +99,9 @@ sed -i "s/@XORG_HWE/$XORG_HWE/" /output/kOS/packages.list
 # sed -i "s/@KERNEL_HEADERS/linux-headers-$KERNEL_FLAVORS/" /output/kOS/packages.list
 
 if [ "$INSTALL_WINE" = "yes" ]; then
-    sed -i "s/@WINE/WINE=yes/" /output/kOS/hooks/normal/020-apps.hook.chroot
+    sed -i "s/#@WINE/WINE=yes/" /output/kOS/hooks/normal/020-apps.hook.chroot
 else
-    sed -i "s/@WINE/WINE=no/" /output/kOS/hooks/normal/020-apps.hook.chroot
+    sed -i "s/#@WINE/WINE=no/" /output/kOS/hooks/normal/020-apps.hook.chroot
 fi
 
 mkdir -p config/package-lists
